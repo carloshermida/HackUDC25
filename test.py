edba7105c4dc4dbe856b3cc7edec26253d70e73a
Carlos Hermida <carloshermay@gmail.com>
2025-02-22 00:57:03 +0100

    init repo

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener las variables de entorno
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+# Crear el cliente de Supabase
+supabase: Client = create_client(url, key)
+
+# Realizar una consulta
+response = (
+    supabase.table("planets")
+    .select("*")
+    .execute()
+)
+
+# Imprimir la respuesta
+print(response)
